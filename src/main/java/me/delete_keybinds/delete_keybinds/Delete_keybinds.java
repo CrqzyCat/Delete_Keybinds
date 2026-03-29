608804d8c211d178cad7bd70b626f5b9323ee79b
crqzycat <crqzycat@gmail.com>
2026-03-30 01:15:10 +0200

    Initial commit

---
+++ src/main/java/me/delete_keybinds/delete_keybinds/Delete_keybinds.java
@@ -0,0 +1,10 @@
+package me.delete_keybinds.delete_keybinds;
+
+import net.fabricmc.api.ModInitializer;
+
+public class Delete_keybinds implements ModInitializer {
+
+    @Override
+    public void onInitialize() {
+    }
+}
