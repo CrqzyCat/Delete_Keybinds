7fbec9f38745c870156b1fd131654518fb78e0f9
crqzycat <4128@htl.rennweg.at>
2026-05-04 20:07:55 +0200

    feat: Add client initialization for DeleteKeybinds

---
+++ src/client/java/me/delete_keybinds/delete_keybinds/client/DeleteKeybindsClientInit.java
@@ -0,0 +1,10 @@
+package me.delete_keybinds.delete_keybinds.client;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class DeleteKeybindsClientInit implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+        // Client-Initialisierung
+    }
+}
